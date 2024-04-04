54c294b8f328a9d13a00bc566e9ddcb0b7120f48
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-03 19:19:36 -0600

    Refactor for time complexity

--- 704_binary_search.js
+++ 704_binary_search.js
@@ -19,12 +19,14 @@ const search = (nums, target) => {
   let end = nums.length - 1;
 
   while (start <= end) {
-    if (nums[start] === target) return start;
-    else if (nums[end] === target) return end;
+    const mid = Math.floor((start + end) / 2);
 
-    start++;
-    end--;
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) start = mid + 1;
+    else end = mid - 1;
   }
 
   return -1;
 };
+
+console.log(search([-1,0,3,5,9,12], 2));
