5dc36d06224715cd99c5ebb341fe9f326e4c163d
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-05 09:31:20 -0600

    Pass tests

---
+++ 824_goat_latin.js
@@ -0,0 +1,39 @@
+// You are given a string sentence that consist of words separated by spaces.
+// Each word consists of lowercase and uppercase letters only.
+
+// We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+// The rules of Goat Latin are as follows:
+
+// If a word begins with a vowel ('a', 'e', 'i', 'o', or 'u'), append "ma" to the end of the word.
+// For example, the word "apple" becomes "applema".
+// If a word begins with a consonant (i.e., not a vowel), remove the first letter and append it to the end, then add "ma".
+// For example, the word "goat" becomes "oatgma".
+// Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+// For example, the first word gets "a" added to the end, the second word gets "aa" added to the end, and so on.
+// Return the final sentence representing the conversion from sentence to Goat Latin.
+
+// Example 1:
+// Input: sentence = "I speak Goat Latin"
+// Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+
+// Example 2:
+// Input: sentence = "The quick brown fox jumped over the lazy dog"
+// Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+
+const toGoatLatin = (sentence) => {
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+  const words = sentence.split(' ');
+  let output = '';
+
+  for (let i = 0; i < words.length; i++) {
+    if (vowels.has(words[i][0].toLowerCase()))
+      output += words[i] + 'ma' + 'a'.repeat(i + 1) + ' ';
+    else
+      output +=
+        words[i].slice(1) + words[i][0] + 'ma' + 'a'.repeat(i + 1) + ' ';
+  }
+
+  return output.trim();
+};
+
+console.log(toGoatLatin('I speak Goat Latin'));
