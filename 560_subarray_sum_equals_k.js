15db3b38ccbccaa71055d8f966b47783a3871950
Cam Johnson <cameron.n.johnson15@gmail.com>
2026-02-09 10:06:27 -0700

    Pass tests

---
+++ 560_subarray_sum_equals_k.js
@@ -0,0 +1,32 @@
+// Given an array of integers nums and an integer k, return the
+// total number of subarrays whose sum equals to k.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+
+function subarraySum(nums, k) {
+  let sum = 0;
+  let output = 0;
+  const map = new Map();
+  map.set(0, 1);
+
+  for (const num of nums) {
+    sum += num;
+
+    if (map.has(sum - k)) output += map.get(sum - k);
+
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+
+  return output;
+}
+
+console.log(subarraySum([1, 1, 1], 2)); // 2
+console.log(subarraySum([1, 2, 3], 3)); // 2
