e1074ae7d908143e66444e2d2605cbd0bd68b6c3
Cam Johnson <cameron.n.johnson15@gmail.com>
2026-02-10 14:01:50 -0700

    Pass tests

---
+++ 3046_split_the_array.js
@@ -0,0 +1,43 @@
+// You are given an integer array nums of even length.
+// You have to split the array into two parts nums1 and nums2 such that:
+
+// nums1.length == nums2.length == nums.length / 2.
+
+// nums1 should contain distinct elements.
+
+// nums2 should also contain distinct elements.
+
+// Return true if it is possible to split the array, and false otherwise.
+
+// Example 1:
+// Input: nums = [1,1,2,2,3,4]
+// Output: true
+// Explanation: One of the possible ways to split nums is nums1 = [1,2,3] and nums2 = [1,2,4].
+
+// Example 2:
+// Input: nums = [1,1,1,1]
+// Output: false
+// Explanation: The only possible way to split nums is nums1 = [1,1] and nums2 = [1,1].
+// Both nums1 and nums2 do not contain distinct elements. Therefore, we return false.
+
+function isPossibleToSplit(nums) {
+  if (nums.length % 2 !== 0) return false;
+
+  const map = new Map();
+  let nums1 = 0;
+  let nums2 = 0;
+
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+
+    if (map.get(num) > 2) return false;
+
+    if (nums1 < nums2) nums1++;
+    else nums2++;
+  }
+
+  return nums1 === nums2 && nums1 === nums.length / 2;
+}
+
+console.log(isPossibleToSplit([1, 1, 2, 2, 3, 4])); // true
+console.log(isPossibleToSplit([1, 1, 1, 1])); // false
---
+++ 796_rotate_string.js
@@ -0,0 +1,23 @@
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+// Example 1:
+// Input: s = "abcde", goal = "cdeab"
+// Output: true
+
+// Example 2:
+// Input: s = "abcde", goal = "abced"
+// Output: false
+
+function rotateString(s, goal) {
+  if (s.length !== goal.length) return false;
+
+  const combined = s + s;
+  return combined.includes(goal);
+}
+
+console.log(rotateString('abcde', 'cdeab')); // true
+console.log(rotateString('abcde', 'abced')); // false
